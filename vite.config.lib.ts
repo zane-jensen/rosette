7545b845b60dfdbe3cb8b96bfaddb5a08074753d
Zane Jensen <zane.jensen@vivint.com>
2026-05-15 15:19:05 -0600

    updated so online demo works on vercel

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from 'path'
+import dts from "vite-plugin-dts";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,5 +11,18 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    dts({insertTypesEntry: true})
   ],
+
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: "rich-editor"
+    },
+    
+    rolldownOptions: {
+      external: ["react", "react-dom", "react/jsx-runtime"]
+    }
+  }
 })
