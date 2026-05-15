2291b561428d7351427e498a070b0549b058a675
Zane Jensen <zane.jensen@vivint.com>
2026-05-15 17:36:42 -0600

    frick yeah

--- vite.config.lib.ts
+++ vite.config.lib.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import babel from '@rolldown/plugin-babel'
 import { resolve } from 'path'
 import dts from "vite-plugin-dts"
 
@@ -9,7 +8,6 @@ import dts from "vite-plugin-dts"
 export default defineConfig({
   plugins: [
     react(),
-    babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
     dts({
       tsconfigPath: "./tsconfig.lib.json",
