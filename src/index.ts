6edd8673fcbd8acef5556a802158285c72ed73a2
Zane Jensen <zane.jensen@vivint.com>
2026-05-18 11:56:25 -0600

    updates to allow for types and factories

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,23 @@
+
+
+// editor
 export {default as Editor} from "./components/Editor/Editor";
-export * as RosetteNode from "./nodes/factories.tsx";
+
+// factories
+export {
+    createTextNode,
+    createListItemNode,
+    createOrderedListNode,
+    createUnorderedListNode,
+} from "./nodes/factories";
+
+// types
+export {NODE_TYPES} from "./nodes/types";
+
+export type {
+    RosetteNode,
+    TextNode,
+    ListItemNode,
+    OrderedListNode,
+    UnorderedListNode
+} from "./nodes/types";
